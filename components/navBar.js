a7a6d63c4fb96198bf062b67ae431239b04afd00
Nick Melms <nmelms92@gmail.com>
2022-11-28 16:24:23 -0500

    added a navbar and background image

---
+++ components/navBar.js
@@ -0,0 +1,38 @@
+const template = `
+ <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div style="max-width: 1200px" class="container">
+    <a class="navbar-brand d-flex" href="/">
+      <img style="height: 40px; width: 40px"src="./assets/burgerIcon.png" >
+      <h2>Burger Bun</h2>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto me-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Menu</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" tabindex="-1" >Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+`;
+
+class Navbar extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = template;
+  }
+}
+
+window.customElements.define("nav-bar", Navbar);
---
+++ index.js
@@ -0,0 +1,4 @@
+import { nav } from "./components/navBar.js";
+
+const navBar = document.querySelector(".navBar");
+navBar.innerHTML = nav;
