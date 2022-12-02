06684d0f1846e250953a540d159630c0242b93a7
Nick Melms <nmelms92@gmail.com>
2022-12-02 18:40:13 -0500

    added new images for carousel

--- components/carousel.js
+++ components/carousel.js
@@ -1,14 +1,14 @@
 const carouselTemplate = `
-<div style="border-radius: 10px" id="carouselExampleControls" class="carousel bg-dark slide m-5 " data-bs-ride="carousel">
+<div style="border-radius: 10px; width: 100%" id="carouselExampleControls" class="d-none d-md-flex justify-content-center carousel  bg-dark slide my-5 " data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img style="height:500px; object-fit: contain" src="./assets/caro1.jpg" class="d-block w-100" alt="...">
+      <img style="margin: 0 auto; max-width:80%; object-fit: contain" src="./assets/caro1.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img style="height:500px; object-fit: contain" src="./assets/caro2.jpg" class="d-block w-100" alt="...">
+      <img style="margin: 0 auto; max-width:80%; object-fit: contain" src="./assets/caro2.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img  style="height:500px; object-fit: contain"src="./assets/caro3.jpg" class="d-block w-100" alt="...">
+      <img  style="margin: 0 auto; max-width:80%; object-fit: contain" src="./assets/caro3.jpg" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev h-100" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
