1189bccfce966db3f43466af42409fa606ed4a10
Nick Melms <nmelms92@gmail.com>
2022-12-07 00:00:35 -0500

    switched all images to webp

--- components/carousel.js
+++ components/carousel.js
@@ -2,13 +2,13 @@ const carouselTemplate = `
 <div style="border-radius: 10px; width: 100%" id="carouselExampleControls" class="d-none d-md-flex justify-content-center carousel  bg-dark slide my-5 " data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img style="margin: 0 auto; max-width:80%; object-fit: contain" src="./assets/caro1.jpg" class="d-block w-100" alt="...">
+      <img style="margin: 0 auto; max-width:80%; object-fit: contain" src="./assets/caro1.webp" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img style="margin: 0 auto; max-width:80%; object-fit: contain" src="./assets/caro2.jpg" class="d-block w-100" alt="...">
+      <img style="margin: 0 auto; max-width:80%; object-fit: contain" src="./assets/caro2.webp" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img  style="margin: 0 auto; max-width:80%; object-fit: contain" src="./assets/caro3.jpg" class="d-block w-100" alt="...">
+      <img  style="margin: 0 auto; max-width:80%; object-fit: contain" src="./assets/caro3.webp" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev h-100" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
