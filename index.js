8f4d9f104ba6a1b1c6446ce7129dda7fa99ff8c5
Nick Melms <nmelms92@gmail.com>
2023-01-06 23:55:16 -0500

    see menu buttons links to menu page

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ import { nav } from "./components/navBar.js";
 
 const navBar = document.querySelector(".navBar");
 navBar.innerHTML = nav;
+
+const menuBtn = document.querySelector(".menuBtn");
+menuBtn.addEventListener("click", () => {
+  console.log("hello");
+});
+console.log("hello");
